0964f22f125cdaa31a34edd3efeb6570345a629b
syymza <syymza@gmail.com>
2015-05-31 18:22:55 +0100

    Step 4

--- meteor-simple-todos.js
+++ meteor-simple-todos.js
@@ -5,7 +5,28 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      // Show newest tasks first
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
     }
   });
 }
